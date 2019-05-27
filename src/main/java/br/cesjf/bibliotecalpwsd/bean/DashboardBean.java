01c5ff1ac3a3d6a86690a7908129a88d3fd4985e
Daniel Meireles <daniel.meireles@outlook.com>
2019-05-27 17:46:11 -0300

    Adição do segundo gráfico

--- src/main/java/br/cesjf/bibliotecalpwsd/bean/DashboardBean.java
+++ src/main/java/br/cesjf/bibliotecalpwsd/bean/DashboardBean.java
@@ -5,7 +5,9 @@
  */
 package br.cesjf.bibliotecalpwsd.bean;
 
+import br.cesjf.bibliotecalpwsd.dao.EmprestimoDAO;
 import br.cesjf.bibliotecalpwsd.dao.ReservaDAO;
+import br.cesjf.bibliotecalpwsd.model.Emprestimo;
 import br.cesjf.bibliotecalpwsd.model.Reserva;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -37,19 +39,20 @@ public class DashboardBean implements Serializable {
     private static final long serialVersionUID = 1L;
     private List<Reserva> reservas;
     private BarChartModel livrosReservados;
+    private List<Emprestimo> emprestimos;
+    private BarChartModel livrosEmprestados;
 
     //construtor
     public DashboardBean() {
         reservas = new ReservaDAO().buscarTodas();
+        emprestimos = new EmprestimoDAO().buscarTodas();
     }
     
     @PostConstruct
     public void init() {
         criarLivrosReservados();
+        criarLivrosEmprestados();
     }
-
-    //getters and setters
-    
     
     public void criarLivrosReservados() {
         livrosReservados = new BarChartModel();
@@ -149,6 +152,105 @@ public class DashboardBean implements Serializable {
  
         livrosReservados.setOptions(options);
     }
+    
+    public void criarLivrosEmprestados() {
+        livrosEmprestados = new BarChartModel();
+        ChartData data = new ChartData();
+         
+        BarChartDataSet barDataSet = new BarChartDataSet();
+        
+        barDataSet.setLabel("Quantidade de Livros Emprestados por Mês ");
+        
+        Calendar data1 = Calendar.getInstance();
+        data1.setTime(new Date());
+        data1.set(Calendar.DAY_OF_MONTH, 1);
+        data1.set(Calendar.MONTH, data1.get(Calendar.MONTH)-3);
+        
+        Calendar data2 = Calendar.getInstance();
+        data2.setTime(new Date());
+        data2.set(Calendar.DAY_OF_MONTH, 1);
+        data2.set(Calendar.MONTH, data2.get(Calendar.MONTH)-2);
+        
+        Calendar data3 = Calendar.getInstance();
+        data3.setTime(new Date());
+        data3.set(Calendar.DAY_OF_MONTH, 1);
+        data3.set(Calendar.MONTH, data3.get(Calendar.MONTH)-1);
+        
+        Calendar data4 = Calendar.getInstance();
+        data4.setTime(new Date());
+        data4.set(Calendar.DAY_OF_MONTH, 1);
+        
+        int mes1 = 0;
+        int mes2 = 0;
+        int mes3 = 0;
+        
+        for(Emprestimo e: emprestimos){
+            Calendar emprestimo = Calendar.getInstance();
+            emprestimo.setTime(e.getDataEmprestimo());
+            if((emprestimo.equals(data1) || emprestimo.after(data1)) && emprestimo.before(data2)) {
+                mes1++;
+            } else if((emprestimo.equals(data2) || emprestimo.after(data2)) && emprestimo.before(data3)) {
+                mes2++;
+            } else if((emprestimo.equals(data3) || emprestimo.after(data3)) && emprestimo.before(data4)) {
+                mes3++;
+            }
+        }
+         
+        List<Number> values = new ArrayList<>();
+        values.add(mes1);
+        values.add(mes2);
+        values.add(mes3);
+        barDataSet.setData(values);
+         
+        List<String> bgColor = new ArrayList<>();
+        bgColor.add("rgba(255, 99, 132, 0.2)");
+        bgColor.add("rgba(255, 159, 64, 0.2)");
+        bgColor.add("rgba(255, 205, 86, 0.2)");
+        barDataSet.setBackgroundColor(bgColor);
+         
+        List<String> borderColor = new ArrayList<>();
+        borderColor.add("rgb(255, 99, 132)");
+        borderColor.add("rgb(255, 159, 64)");
+        borderColor.add("rgb(255, 205, 86)");
+        barDataSet.setBorderColor(borderColor);
+        barDataSet.setBorderWidth(1);
+         
+        data.addChartDataSet(barDataSet);
+         
+        List<String> labels = new ArrayList<>();
+        labels.add(mes(data1.get(Calendar.MONTH)));
+        labels.add(mes(data2.get(Calendar.MONTH)));
+        labels.add(mes(data3.get(Calendar.MONTH)));
+        data.setLabels(labels);
+        livrosEmprestados.setData(data);
+         
+        //Options
+        BarChartOptions options = new BarChartOptions();
+        CartesianScales cScales = new CartesianScales();
+        CartesianLinearAxes linearAxes = new CartesianLinearAxes();
+        CartesianLinearTicks ticks = new CartesianLinearTicks();
+        ticks.setBeginAtZero(true);
+        linearAxes.setTicks(ticks);
+        cScales.addYAxesData(linearAxes);
+        options.setScales(cScales);
+         
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Gráfico de Livros Emprestados nos 3 Últimos Meses");
+        options.setTitle(title);
+ 
+        Legend legend = new Legend();
+        legend.setDisplay(true);
+        legend.setPosition("top");
+        LegendLabel legendLabels = new LegendLabel();
+        legendLabels.setFontStyle("bold");
+        legendLabels.setFontColor("#2980B9");
+        legendLabels.setFontSize(24);
+        legend.setLabels(legendLabels);
+        options.setLegend(legend);
+ 
+        livrosEmprestados.setOptions(options);
+    }
 
     public List<Reserva> getReservas() {
         return reservas;
@@ -164,6 +266,22 @@ public class DashboardBean implements Serializable {
 
     public void setLivrosReservados(BarChartModel livrosReservados) {
         this.livrosReservados = livrosReservados;
+    }
+
+    public List<Emprestimo> getEmprestimos() {
+        return emprestimos;
+    }
+
+    public void setEmprestimos(List<Emprestimo> emprestimos) {
+        this.emprestimos = emprestimos;
+    }
+
+    public BarChartModel getLivrosEmprestados() {
+        return livrosEmprestados;
+    }
+
+    public void setLivrosEmprestados(BarChartModel livrosEmprestados) {
+        this.livrosEmprestados = livrosEmprestados;
     }
     
     public String mes(int m) {
