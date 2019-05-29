61a3b07651341b52c764bf514564c858214f0f44
Daniel Meireles <daniel.meireles@outlook.com>
2019-05-29 18:51:03 -0300

    Adição do restante dos relatórios

--- src/main/java/br/cesjf/bibliotecalpwsd/bean/RelatorioBean.java
+++ src/main/java/br/cesjf/bibliotecalpwsd/bean/RelatorioBean.java
@@ -5,8 +5,10 @@
  */
 package br.cesjf.bibliotecalpwsd.bean;
 
-import br.cesjf.bibliotecalpwsd.reports.Relatorio;
+import br.cesjf.bibliotecalpwsd.util.Relatorio;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 import org.omnifaces.cdi.ViewScoped;
 import javax.inject.Named;
 /**
@@ -19,9 +21,38 @@ public class RelatorioBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
     
-    public void gerarRelatorio01() {
+    private String relatorio;
+    
+    private Map<String, String> tipos;
+
+    public RelatorioBean() {
+        tipos = new HashMap<String, String>();
+        tipos.put("Relatório de Livros/Exemplares", "Relatorio_01");
+        tipos.put("Relatório de Livros Emprestados", "Relatorio_02");
+        tipos.put("Relatório de Livros Reservados", "Relatorio_03");
+        tipos.put("Relatório de Usuários Cadastrados", "Relatorio_04");
+        tipos.put("Relatório de Livros com Atraso na Devolução", "Relatorio_05");
+    }
+
+    public String getRelatorio() {
+        return relatorio;
+    }
+
+    public void setRelatorio(String relatorio) {
+        this.relatorio = relatorio;
+    }
+
+    public Map<String, String> getTipos() {
+        return tipos;
+    }
+
+    public void setTipos(Map<String, String> tipos) {
+        this.tipos = tipos;
+    }
+    
+    public void gerarRelatorio() {
         Relatorio relatorio = new Relatorio();
-        relatorio.getRelatorio("Relatorio_01");
+        relatorio.getRelatorio(this.relatorio);
     }
     
 }
--- src/main/java/br/cesjf/bibliotecalpwsd/util/Relatorio.java
+++ src/main/java/br/cesjf/bibliotecalpwsd/util/Relatorio.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.cesjf.bibliotecalpwsd.reports;
+package br.cesjf.bibliotecalpwsd.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
