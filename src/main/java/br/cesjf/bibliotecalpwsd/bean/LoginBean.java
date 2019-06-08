316a519adb87eb1432062591f0d70c314f7b3329
Daniel Meireles <daniel.meireles@outlook.com>
2019-06-08 17:55:52 -0300

    Ajustes gerais

--- src/main/java/br/cesjf/bibliotecalpwsd/bean/LoginBean.java
+++ src/main/java/br/cesjf/bibliotecalpwsd/bean/LoginBean.java
@@ -8,24 +8,41 @@ package br.cesjf.bibliotecalpwsd.bean;
 import br.cesjf.bibliotecalpwsd.dao.LoginDAO;
 import br.cesjf.bibliotecalpwsd.dao.UsuarioDAO;
 import br.cesjf.bibliotecalpwsd.model.Usuario;
+import com.github.adminfaces.template.session.AdminSession;
+import org.omnifaces.util.Faces;
+
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Specializes;
+import javax.inject.Named;
+import java.io.IOException;
 import java.io.Serializable;
+import com.github.adminfaces.template.config.AdminConfig;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
-import org.omnifaces.cdi.ViewScoped;
-import javax.inject.Named;
 /**
  *
  * @author dmeireles
  */
 @Named
-@ViewScoped
-public class LoginBean implements Serializable {
+@SessionScoped
+@Specializes
+public class LoginBean extends AdminSession implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private String nome;
     private String usuario;
     private String senha;
+    
+    @Inject
+    private AdminConfig adminConfig;
+    
+    @Override
+    public boolean isLoggedIn() {
+
+        return nome != null;
+}
 
     public String getUsuario() {
         return usuario;
@@ -51,24 +68,31 @@ public class LoginBean implements Serializable {
         this.nome = nome;
     }
     
-    public String login() {        
+    public void login() throws IOException {        
         if (LoginDAO.login(usuario, senha)) {
             Usuario u = UsuarioDAO.getInstance().buscar(usuario);
             HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
             session.setAttribute("usuario", u);
             session.setAttribute("nome", u.getNome());
             nome = u.getNome();
-            return "index";
+            setIsLoggedIn(true);
+            Faces.getExternalContext().getFlash().setKeepMessages(true);
+            Faces.redirect(adminConfig.getIndexPage());
         } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Usuário ou senha inválido!", "Por favor tente novamente!"));
-            return "login";
+            Faces.getExternalContext().getFlash().setKeepMessages(true);
+            Faces.redirect(adminConfig.getLoginPage());
         }
     }
     
-    public String logout() {
+    public void logout() throws IOException {
         HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
         session.invalidate();
-        return "login";
+        nome = null;
+        setIsLoggedIn(false);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Logout efetuado com sucesso!", ""));
+        Faces.getExternalContext().getFlash().setKeepMessages(true);
+        Faces.redirect(adminConfig.getLoginPage());
     }
     
 }
